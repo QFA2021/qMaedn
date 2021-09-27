99377b4ea840181e1675ff6831b1e7c0cf375fe5
Daniel Buecheler <dabch@posteo.de>
2021-09-27 15:31:07 +0200

    add dice stuff

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import pyglet
 from game import *
 from pyglet.window import mouse
+import random
+
 from imageload import load_pngs
 from util import Color
 import validation
 
 global board, window
+
+
+
+class Controls:
+    
+    def __init__(self, x, y):
+        self.shapes = []
+        self.x = x
+        self.y = y
+        self.diceval = 1
+
+    def throw_die(self):
+        self.diceval = random.randint(1,6)
+
+    def update_batch(self, batch):
+        label_text = f'Dice roll: {self.diceval}'
+        label = pyglet.text.Label(label_text, font_name='Arial', font_size=36, x=self.x, y=self.y,
+                                          color=(0, 0, 0, 255), anchor_x='center', anchor_y='center', batch=batch)
+        self.shapes.append(label)
 
 
 if __name__ == "__main__":
@@ -14,12 +35,15 @@ if __name__ == "__main__":
     pyglet.gl.glClearColor(255, 255, 255, 1.0)
     board_batch = pyglet.graphics.Batch()
     stone_batch = pyglet.graphics.Batch()
+    control_batch = pyglet.graphics.Batch()
 
     load_pngs()
 
     board = Board(window)
     board.initialize_board_batch(board_batch)
     board.update_stone_batch(stone_batch)
+    
+    controls = Controls(window.width//2, 950)
 
 
     @window.event
@@ -53,7 +77,10 @@ if __name__ == "__main__":
         stone_batch=pyglet.graphics.Batch()
         board.update_stone_batch(stone_batch)
         stone_batch.draw()
+        controls.update_batch(control_batch)
+        control_batch.draw()
 
     pyglet.app.run()
+
 
 
